0c7b7350e1478a38622e2cfaa9b3e31bf23a93f3
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-04-27 20:01:11 -0400

    Wrapped gui in a class and enter now submits the forms

--- authGui.py
+++ authGui.py
@@ -1,49 +1,60 @@
 from tkinter import *
 from pythonClient import login,logout
 
-def loginUser(pin):
-	msg.configure(text=login(pin))
-
-def logOutUser(pin):
-    msg.configure(text=logout(pin))
-
-def chooser():
-    global pin,loggedIn
-
-    pinLog = pin.get()
-    
-    if pinLog in loggedIn:
-        logOutUser(pinLog)
-    else:
-        loginUser(pinLog)
-        loggedIn.append(pinLog)
-
-global loggedIn
-loggedIn = []	
-
-# Make a tkinter form for registering users using username and pin
-# Display a sucess message if the user is registered else display failure message
-window = Tk()
-window.title("Log Hours")
-window.geometry("500x500")
-
-msg = Label(window)
-msg.grid(row=5, column=1)
 
 
-# Make a label for pin
-lbl_pin = Label(window, text="Pin")
-lbl_pin.grid(column=0, row=0)
+class LogHourForm:
+
+    def __init__(self):
+        self.loggedIn = []	
+
+        # Make a tkinter form for registering users using username and pin
+        # Display a sucess message if the user is registered else display failure message
+        window = Tk()
+        window.title("Log Hours")
+        window.geometry("500x500")
+
+        window.bind("<Return>",lambda x: self.chooser())
+
+        self.msg = Label(window)
+        self.msg.grid(row=5, column=1)
 
 
-# Make a textbox for pin
-global pin
-pin = Entry(window, width=10)
-pin.grid(column=1, row=0)
+        # Make a label for pin
+        lbl_pin = Label(window, text="Pin")
+        lbl_pin.grid(column=0, row=0)
 
 
-btn_submit = Button(window, text="Submit", command=chooser)
-btn_submit.grid(column=1, row=4)
+        # Make a textbox for pin
+        self.pin = Entry(window, width=10)
+        self.pin.grid(column=1, row=0)
 
-window.mainloop()
+
+        btn_submit = Button(window, text="Submit", command=self.chooser)
+        btn_submit.grid(column=1, row=4)
+
+        window.mainloop()
+
+    def loginUser(self,pin):
+        self.msg.configure(text=login(pin))
+
+    def logOutUser(self,pin):
+        self.msg.configure(text=logout(pin))
+
+    def chooser(self):
+
+        pinLog = self.pin.get()
+        
+        if pinLog in self.loggedIn:
+            self.logOutUser(pinLog)
+            self.pin.delete(0,END)
+        else:
+            self.loginUser(pinLog)
+            self.loggedIn.append(pinLog)
+            self.pin.delete(0,END)
+
+
+# Start the application
+LogHourForm()
+
 
--- registerGui.py
+++ registerGui.py
@@ -1,47 +1,43 @@
 from tkinter import *
 from pythonClient import register
 
-def registerUser():
-	global username,pin
+class RegisterForm:
+	def __init__(self):
+		window = Tk()
+		window.title("Register")
+		window.geometry("500x500")
 
-	user = username.get()
-	pinLog = pin.get()
-	msg.configure(text=register(user,pinLog))
-	
+		self.msg = Label(window)
+		self.msg.grid(row=5, column=1)
 
-# Make a tkinter form for registering users using username and pin
-# Display a sucess message if the user is registered else display failure message
-window = Tk()
-window.title("Register")
-window.geometry("500x500")
+		# Make a label for username
+		lbl_username = Label(window, text="Username")
+		lbl_username.grid(column=0, row=0)	
+			
+		# Make a label for pin
+		lbl_pin = Label(window, text="Pin")
+		lbl_pin.grid(column=0, row=1)
 
-msg = Label(window)
-msg.grid(row=5, column=1)
+		# Make a textbox for username
+		self.username = Entry(window, width=10)
+		self.username.grid(column=1, row=0)
 
-# Make a label for username
-lbl_username = Label(window, text="Username")
-lbl_username.grid(column=0, row=0)	
-	
-# Make a label for pin
-lbl_pin = Label(window, text="Pin")
-lbl_pin.grid(column=0, row=1)
-
-# Make a textbox for username
-global username
-username = Entry(window, width=10)
-username.grid(column=1, row=0)
-
-# Make a textbox for pin
-global pin
-pin = Entry(window, width=10)
-pin.grid(column=1, row=1)
+		# Make a textbox for pin
+		self.pin = Entry(window, width=10)
+		self.pin.grid(column=1, row=1)
 
 
-btn_submit = Button(window, text="Submit", command=registerUser)
-btn_submit.grid(column=1, row=4)
+		btn_submit = Button(window, text="Submit", command=self.registerUser)
+		btn_submit.grid(column=1, row=4)
+
+		window.mainloop()
+
+	def registerUser(self):
+
+		user = self.username.get()
+		pinLog = self.pin.get()
+		self.msg.configure(text=register(user,pinLog))
 
 
-
-
-window.mainloop()
-
+# Start the application
+RegisterForm()
