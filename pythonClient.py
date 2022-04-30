9e9f56138469057a785b89aef14fb8e62ef8af35
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2022-04-30 13:01:38 -0400

    Added new local storage functionality

--- pythonClient.py
+++ pythonClient.py
@@ -1,20 +1,41 @@
 import datetime as dt
 from csv import writer, DictReader
 import sheets
-import localStorage
+import _thread as thread
+import time
 
 
-useLocalStorage = False
-try:
-    sheetName = "AttendanceOffSeason2022"
-    sheetObject = sheets.sheets(sheetName)
-except ValueError:
-    useLocalStorage = True
+sheetName = "AttendanceOffSeason2022"
+sheetObject = sheets.sheets(sheetName)
 
 global tempHour
 tempHour = {
     # 'pin': loginTime
 }
+
+def waitForWriteLine(line):
+    while (True):
+        try:
+            with open("process.txt", "a") as f:
+                f.write(line+"\n")
+            return
+        except:
+            print("File blocked")
+        time.sleep(0.1)
+
+def getUserFromPin(pin):
+    '''
+    Returns the name of the user from the pin
+    Returns None if the pin is not found
+    '''
+
+    with open('pins.csv','r') as file:
+        csv_reader = DictReader(file)
+        for row in csv_reader:
+            if row['pin'] == pin:
+                return row['name']
+
+    return None
 
 def login(pin):
     user = getUserFromPin(pin)
@@ -22,7 +43,7 @@ def login(pin):
     if user is None:
         return "Invalid Pin"
     else:
-        # sheetObject.login(user)
+        sheetObject.login(user)
         tempHour[pin] = dt.datetime.now()
         return "Logged in "+user+" at "+str(tempHour[pin])
 
@@ -43,34 +64,22 @@ def logout(pin, ignoreHours=False):
             return "Invalid Pin"
         else:
             try:
-                # sheetObject.logout(user)
+                sheetObject.logout(user)
                 currentSeconds = (dt.datetime.now() - tempHour[pin]).total_seconds()
 
-                if not useLocalStorage:
-                    sheetObject.sendHours(user ,round(currentSeconds/3600,2))
-                else: 
-                    localStorage.sendHours(user, round(currentSeconds/3600,2))
+                waitForWriteLine(
+                    user
+                    +","
+                    +str(round(currentSeconds/3600,2))
+                    +","
+                    +str(dt.datetime.now())
+                )
+
                 tempHour.pop(pin)
             except KeyError:
                 return "Not logged out, you are not logged in"
             return "Logged out "+user+" at "+str(dt.datetime.now())
 
-
-
-
-def getUserFromPin(pin):
-    '''
-    Returns the name of the user from the pin
-    Returns None if the pin is not found
-    '''
-
-    with open('pins.csv','r') as file:
-        csv_reader = DictReader(file)
-        for row in csv_reader:
-            if row['pin'] == pin:
-                return row['name']
-
-    return None
 
 def register(name,pin):
     try:
@@ -81,5 +90,6 @@ def register(name,pin):
     except Exception as e:
         print(e)
         return "Error in registering: "+e
+
 
 
